9b83870e9df93ff166e20731cf596d36e8222dc9
Beth Crane <abethcrane@gmail.com>
2019-08-07 20:51:55 +0100

    Replace ResetScene with SceneLoader

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
