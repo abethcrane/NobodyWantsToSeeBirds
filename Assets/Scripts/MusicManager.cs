42e17512c652013cae71ac8d2e14ed00b768cf84
Beth Crane <abethcrane@gmail.com>
2019-08-07 21:17:33 +0100

    Update music manager to have cross fade, and add in sound files

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
@@ -24,7 +25,32 @@ public class MusicManager : MonoBehaviour
 
     private void OnGameOver()
     {
-        _themeMusicLoop.Stop();
-        _holdMusic.Play();
+        StartCoroutine(CrossFade(_themeMusicLoop, _holdMusic));
+    }
+
+    private static IEnumerator CrossFade(AudioSource oldMusic, AudioSource newMusic, float FadeTime=2f)
+    {
+        float startVolume = oldMusic.volume;
+        float endVolume = newMusic.volume;
+        newMusic.volume = 0;
+        newMusic.Play();
+
+        while (oldMusic.volume > 0 || newMusic.volume < endVolume)
+        {
+            if (oldMusic.volume > 0)
+            {
+                oldMusic.volume -= startVolume * Time.deltaTime / FadeTime;
+            }
+
+            if (newMusic.volume < endVolume)
+            {
+                newMusic.volume += endVolume * Time.deltaTime / FadeTime;
+            }
+
+            yield return null;
+        }
+
+        oldMusic.Stop();
+        oldMusic.volume = startVolume;
     }
 }
