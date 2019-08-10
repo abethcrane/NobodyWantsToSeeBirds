1fc49260f7be3e20086ed2ac12fc32c090f93128
Beth Crane <abethcrane@gmail.com>
2019-08-10 20:12:08 +0100

    Add in a seconds-alive display

---
+++ Assets/Scripts/TimeDisplay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(TMPro.TextMeshProUGUI))]
+public class TimeDisplay : MonoBehaviour
+{
+    private TMPro.TextMeshProUGUI _text;
+
+    private void Awake()
+    {
+        _text = GetComponent<TMPro.TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        if (Main.Instance.IsGameActive)
+        {
+            _text.text = "Seconds: " + Time.timeSinceLevelLoad.ToString("0.0");
+        }
+    }
+}
