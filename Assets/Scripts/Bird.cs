6d1905b5da95ecb7f8faf8a956b8a2664fc70995
Beth Crane <abethcrane@gmail.com>
2020-04-01 23:14:25 -0400

    Add in new bird censor images, and modify the animations to use that instead.
    Each bird sprite now has its own collision mesh too (for the slight differences in wings).

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
@@ -11,7 +12,10 @@ public class Bird : MonoBehaviour
     private Animator _anim;
     private bool _isVisibleToGrampa = true;
     private bool _hasLostLife = false;
-    private OnScreenVisibilityEventer _sprite;
+    private OnScreenVisibilityEventer[] _sprites;
+    private Renderer[] _spriteRenderers;
+    private bool _checkForOffscreen = false;
+    private bool _checkingOffscreen = false;
 
     public void Reset()
     {
@@ -21,15 +25,30 @@ public class Bird : MonoBehaviour
             _anim.SetBool("IsVisible", _isVisibleToGrampa);
         }
         _hasLostLife = false;
+        _checkForOffscreen = false;
+        _checkingOffscreen = false;
     }
 
     private void Start()
     {
         _anim = GetComponentInChildren<Animator>();
-        _sprite = GetComponentInChildren<OnScreenVisibilityEventer>();
-        _sprite.BecameInvisible += OnBecameInvisible;
+        _sprites = GetComponentsInChildren<OnScreenVisibilityEventer>();
+        _spriteRenderers = GetComponentsInChildren<Renderer>();
+        foreach (var sprite in _sprites)
+        {
+            sprite.BecameInvisible += OnBecameInvisible;
+            sprite.BecameVisible += OnBecameVisible;
+        }
     }
 
+    private void Update()
+    {
+        if (_checkForOffscreen && !_checkingOffscreen)
+        {
+            _checkingOffscreen = true;
+            StartCoroutine(CheckOffscreen());
+        }
+    }
 
     private void OnMouseDown()
     {
@@ -63,9 +82,45 @@ public class Bird : MonoBehaviour
         }
     }
 
+    // This offscreen system became a little more complex than I would have liked, given we have multiple children sprites
+    // It'd be nice to make this part of OnScreenVisiblityEventer, but, eh...
+    // So basically when we become invisible we set the check to true, when we become visible we set it back to false
+    // Every update, if the check is on then we trigger an actual check event and indicate that
+    // The actual check waits 1 second, and then if the check is on it loops through all children eventers and if they're all off it marks us as invisible
     private void OnBecameInvisible()
     {
-        Main.Instance.OffScreen();
-        gameObject.SetActive(false);
+        _checkForOffscreen = true;
+    }
+
+    private void OnBecameVisible()
+    {
+        _checkForOffscreen = false;
+    }
+
+    private IEnumerator CheckOffscreen() 
+    {
+        // Wait a second before we check if the children are all invisible so that we're not too trigger happy
+        yield return new WaitForSeconds(1);
+
+        if (_checkForOffscreen)
+        {
+            var isVisible = false;
+            foreach (var sprite in _spriteRenderers)
+            {
+                if (sprite.isVisible)
+                {
+                    isVisible = true;
+                    continue;
+                }
+            }
+
+            if (!isVisible)
+            {
+                _checkForOffscreen = false;
+                Main.Instance.OffScreen();
+                gameObject.SetActive(false);
+            }
+        }
+        _checkingOffscreen  = false;
     }
 }
--- Assets/Scripts/OnScreenVisibilityEventer.cs
+++ Assets/Scripts/OnScreenVisibilityEventer.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 public class OnScreenVisibilityEventer : MonoBehaviour
 {
     public event Action BecameInvisible;
+    public event Action BecameVisible;
 	
 	private void OnBecameInvisible()
 	{
         BecameInvisible?.Invoke();
+	}
+	
+	private void OnBecameVisible()
+	{
+        BecameVisible?.Invoke();
 	}
 }
