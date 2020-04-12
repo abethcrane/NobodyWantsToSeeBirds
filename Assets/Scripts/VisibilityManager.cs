715403d91fec15b133ecee28da7fd4a1959c02f5
Beth Crane <abethcrane@gmail.com>
2020-04-12 18:28:41 -0400

    Check in changes to visibility manager to add in eventing for the top/bottom of the screen
    Not the left, because birds start off the left

--- Assets/Scripts/VisibilityManager.cs
+++ Assets/Scripts/VisibilityManager.cs
@@ -6,26 +6,47 @@ public class VisibilityManager : MonoBehaviour
     public event Action ObjectOffScreen;
 
     private Camera _camera;
-    private float _objectLeftSideOffset = 0f;
+    private float _objectWidthOffset = 0f;
+    private float _objectHeightOffset = 0f;
     private void Start()
     {
         _camera = Camera.main;
         var collider = GetComponentInChildren<Collider2D>();
 
+        // We calculate these so that we can wait  until the whole object has gone off screen, not just the center.
         // We need to cache it here because it might be inactive when they leave the screen (and return 0 haha)
         // In this game it's okay because sizes don't change
-        _objectLeftSideOffset = -collider.bounds.extents.x;
+        _objectWidthOffset = collider.bounds.extents.x;
+        _objectHeightOffset = collider.bounds.extents.y;
     }
 
     // No need to check if the game is paused
     private void FixedUpdate()
     {
-        var rightEdgeOfScreen = _camera.ViewportToWorldPoint(new Vector2(1, 1)).x;
-        
         // Wait until the whole object has gone off screen
-        var leftEdgeOfObj = gameObject.transform.position.x + _objectLeftSideOffset;
+        var rightEdgeOfScreen = _camera.ViewportToWorldPoint(new Vector2(1, 1)).x;        
+        var leftEdgeOfObj = gameObject.transform.position.x - _objectWidthOffset;
 
         if (leftEdgeOfObj > rightEdgeOfScreen)
+        {
+            ObjectOffScreen?.Invoke();
+        }
+
+        
+        // Wait until the whole object has gone off screen
+        var bottomEdgeOfScreen = _camera.ViewportToWorldPoint(new Vector2(1, 0)).y;
+        var topofObject = gameObject.transform.position.y + _objectHeightOffset;
+
+        if (topofObject < bottomEdgeOfScreen)
+        {
+            ObjectOffScreen?.Invoke();
+        }
+
+        // Wait until the whole object has gone off screen
+        var topEdgeOfScreen = _camera.ViewportToWorldPoint(new Vector2(1, 1)).y;        
+        var bottomOfObject = gameObject.transform.position.y - _objectHeightOffset;
+
+        if (bottomOfObject > topEdgeOfScreen)
         {
             ObjectOffScreen?.Invoke();
         }
