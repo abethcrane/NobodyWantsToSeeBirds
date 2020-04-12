583c8be0864364fe53d4767d16912a140d8dafe7
Beth Crane <abethcrane@gmail.com>
2020-04-12 18:23:56 -0400

    Add in sound effects for air balloon

--- Assets/Scripts/Balloon.cs
+++ Assets/Scripts/Balloon.cs
@@ -1,10 +1,21 @@
-﻿using UnityEngine;
+﻿
+using System.Collections;
+using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
 public class Balloon : MonoBehaviour
 {
     [SerializeField]
     private Transform _child = null;
+
+    [SerializeField]
+    private AudioSource _audioSourcePop = null;
+
+    [SerializeField]
+    private AudioSource _audioSourceHiss = null;
+
+    [SerializeField]
+    private AudioSource _audioSourceThump = null;
 
     private Animator _anim;
     private bool _isPopped = false;
@@ -31,6 +42,10 @@ public class Balloon : MonoBehaviour
         }
 
         GetComponent<Rigidbody2D>().gravityScale = 0f;
+
+        _audioSourcePop.Stop();
+        _audioSourceHiss.Stop();
+        _audioSourceThump.Stop();
     }
 
     private void Start()
@@ -47,37 +62,70 @@ public class Balloon : MonoBehaviour
     {
         if (Main.Instance.IsGameActive && !_isPopped)
         {
-            _anim.SetBool("IsPopped", true);
-            _isPopped = true;
-            // Turn gravity on
-            GetComponent<Rigidbody2D>().gravityScale = 1f;
-            // Play a popping sound
-            // After that, play a falling sound            
+            OnPop();
         }
     }
 
-    private void OnOffScreen()
-    {
-        gameObject.SetActive(false);
-    }
 
     // When it hits the ground we make an explosion and that's what loses us a life
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (_isPopped && !_hasLostLife)
-        {
-            // Play an explosion sound
-            Main.Instance.Explosion();
-            _hasLostLife = true;
-            gameObject.SetActive(false);
+        { 
+            OnCrash();
         }
     }
 
+    private void OnPop()
+    {
+        _anim.SetBool("IsPopped", true);
+        _isPopped = true;
+
+        // Turn gravity on
+        GetComponent<Rigidbody2D>().gravityScale = 20f;
+
+        // Play a popping sound and a hissing sound (at the same time)
+        _audioSourcePop.Play();
+        _audioSourceHiss.Play();
+    }
+
+    private void OnCrash()
+    {
+        // Play an explosion sound
+        Main.Instance.Explosion();
+        _audioSourceThump.Play();
+        _hasLostLife = true;
+        StartCoroutine ("DisableWhenNotPlaying");
+    }
+
     private void OnPauseToggled(bool isPaused) {
-        //_anim.enabled = !isPaused;
         // If it's paused, set speed to 0, else to 1
         // This only affects popped - fly can proceed as normal
         _anim.SetFloat("SpeedMultiplier", isPaused ? 0f : 1f);
+
         // also pause the sounds
+        if (isPaused)
+        {
+            _audioSourcePop.Pause();
+            _audioSourceHiss.Pause();
+            _audioSourceThump.Pause();
+        }
+        else
+        {
+            _audioSourcePop.UnPause();
+            _audioSourceHiss.UnPause();
+            _audioSourceThump.UnPause();
+        }
+    }
+
+    private void OnOffScreen()
+    {
+        StartCoroutine ("DisableWhenNotPlaying");
+    }
+
+    private IEnumerator DisableWhenNotPlaying()
+    {
+        yield return new WaitWhile (() => _audioSourceThump.isPlaying);
+        gameObject.SetActive(false);
     }
 }
